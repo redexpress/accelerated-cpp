17190656e971d7ae9225e6ef03a778c108305e47
redppl@hotmail.com <redppl@hotmail.com>
2026-06-17 10:02:58 +0800

    update

--- source/ch13/Core.cpp
+++ source/ch13/Core.cpp
@@ -51,3 +51,36 @@ double Grad::grade() const
 {
     return min(Core::grade(), thesis);
 }
+
+
+istream& Student_info::read(std::istream& is)
+{
+    delete cp;
+
+    char ch;
+    is >> ch;
+
+    if (ch=='U') {
+        cp=new Core(is);
+    } else {
+        cp=new Grad(is);
+    }
+    return is;
+}
+
+Student_info::Student_info(const Student_info& s): cp(0)
+{
+    if (s.cp) cp = s.cp->clone();
+}
+
+Student_info& Student_info::operator=(const Student_info& s)
+{
+    if (&s != this) {
+        delete cp;
+        if (s.cp)
+            cp = s.cp->clone();
+        else
+            cp = 0;
+    }
+    return *this;
+}
--- source/ch13/Core.h
+++ source/ch13/Core.h
@@ -4,7 +4,11 @@
 #include <istream>
 #include <vector>
 
+class Student_info;
+
+
 class Core {
+    friend class Student_info;
 public:
     Core():midterm(0), final(0) {}
     Core(std::istream& is) {read(is);}
@@ -15,6 +19,8 @@ protected:
     std::istream& read_common(std::istream&);
     double midterm, final;
     std::vector<double> homework;
+
+    virtual Core* clone() const { return new Core(*this); }
 private:
     std::string n;
 };
@@ -22,14 +28,46 @@ private:
 bool compare(const Core&, const Core&);
 bool compare_core_ptrs(const Core*, const Core*);
 
+
 class Grad : public Core {
 public:
     Grad():thesis(0){}
     Grad(std::istream& is){ read(is); }
     double grade() const;
     std::istream& read(std::istream& is);
+protected:
+    Grad* clone() const { return new Grad(*this); }
 private:
     double thesis;
+};
+
+
+class Student_info {
+public:
+    Student_info(): cp(0) { }
+    Student_info(std::istream& is): cp(0) { read(is); }
+    Student_info(const Student_info&);
+    Student_info& operator=(const Student_info&);
+    ~Student_info() { delete cp; }
+
+    std::istream& read(std::istream&);
+
+    std::string name() const {
+        if (cp) return cp->name();
+        else throw std::runtime_error("uninitialized Student");
+    }
+    double grade() const {
+        if (cp) return cp->grade();
+        else throw std::runtime_error("uninitialized Student");
+    }
+
+    static bool compare(const Student_info& s1,
+                        const Student_info& s2) {
+        return s1.name() < s2.name();
+    }
+
+private:
+    Core* cp;
 };
 
 #endif
